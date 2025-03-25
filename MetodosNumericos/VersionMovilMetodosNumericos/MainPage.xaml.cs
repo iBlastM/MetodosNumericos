3fd19f12a4c1b398770d4299be79810cab2a1c23
Miguel <bodoque0@hotmail.com>
2025-03-24 20:56:51 -0600

    Agregada la version movil.

---
+++ MetodosNumericos/VersionMovilMetodosNumericos/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace VersionMovilMetodosNumericos;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MetodosNumericos/VersionMovilMetodosNumericos/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace VersionMovilMetodosNumericos;
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
